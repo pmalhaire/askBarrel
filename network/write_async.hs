140d26fd536135cd265050457d984bcb777cbe3a
pmalhaire <pierre.malhiare@gmail.com>
2019-05-01 17:24:24 +0200

    cleanup and comment add more comment in hs file add help for the build script

--- network/write_async.hs
+++ network/write_async.hs
@@ -1,47 +1,64 @@
-{-# LANGUAGE OverloadedStrings #-}
--- Echo client program
 module Main (main) where
 
 import qualified Control.Exception as E
 import qualified Data.ByteString.Char8 as C
-import Network.Socket (AddrInfo, Socket, addrSocketType, addrAddress, addrProtocol, addrFamily, connect, socket, getAddrInfo, defaultHints, close, withSocketsDo, SocketType( Stream ))
+import qualified Network.Socket as NS
 import Network.Socket.ByteString (recv, sendAll)
 import Control.Concurrent
 
+-- avoid mixing :
+-- putStrLn :: String -> IO()
+-- C.putStrLn :: ByteString -> IO()
+import Prelude hiding (putStrLn)
+
+type Msg = C.ByteString
+
+resolve :: String -> String -> IO NS.AddrInfo
 resolve host port = do
-    let hints = defaultHints { addrSocketType = Stream }
-    addr:_ <- getAddrInfo (Just hints) (Just host) (Just port)
+    let hints = NS.defaultHints { NS.addrSocketType = NS.Stream }
+    addr:_ <- NS.getAddrInfo
+        (Just hints)
+        (Just host)
+        (Just port)
     return addr
 
-open :: AddrInfo -> IO Socket
+open :: NS.AddrInfo -> IO NS.Socket
 open addr = do
-    sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
-    connect sock $ addrAddress addr
+    sock <- NS.socket
+        (NS.addrFamily addr)
+        (NS.addrSocketType addr)
+        (NS.addrProtocol addr)
+    NS.connect sock $ NS.addrAddress addr
     return sock
 
-talk :: Socket -> IO ()
-talk sock = do
-    putStrLn "send your_content and wait for response"
-    content <- C.getLine
-    ack <- run sock content
-    C.putStrLn ack
-    talk sock
-
-run :: Socket -> C.ByteString -> IO C.ByteString
+run :: NS.Socket -> C.ByteString -> IO C.ByteString
 run sock content = do
     _ <- sendAll sock content
+    -- wait for response asyncronously
     forkIO( do
         resp <- recv sock 100
         C.putStrLn resp )
     return $ C.pack "sent"
-    
-main = do
-    addr <- resolve "127.0.0.1" "3000"
-    chan <- newChan        -- notice that newChan :: IO (Chan a)
-    mainLoop addr chan     -- pass it into the loop
-    
-type Msg = C.ByteString
 
-mainLoop :: AddrInfo -> Chan Msg -> IO ()   -- See how Chan now uses Msg.
-mainLoop addr chan = do
-    E.bracket (open addr) close talk
+talk :: NS.Socket -> IO ()
+talk sock = do
+    -- prompt string
+    C.putStr $ C.pack ">>>"
+    -- get string from command line
+    content <- C.getLine
+    -- send data
+    ack <- run sock content
+    -- show ack
+    C.putStrLn ack
+
+    talk sock
+
+mainLoop :: NS.AddrInfo -> IO ()
+mainLoop addr = do
+    E.bracket (open addr) NS.close talk
+
+main :: IO()
+main = do
+    C.putStrLn $ C.pack "send your_content and wait for response"
+    addr <- resolve "127.0.0.1" "3000"
+    mainLoop addr
